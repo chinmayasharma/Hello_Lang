ec434e3ce0d445c55f8b4add35d856bf9562f9c6
Jack Henhapl <jackah2@illinois.edu>
2017-11-03 06:59:09 -0500

    Added ability to change profile

--- app/src/main/java/jackah2/hellolangfeed/FilterChooser.java
+++ app/src/main/java/jackah2/hellolangfeed/FilterChooser.java
@@ -1,6 +1,8 @@
 package jackah2.hellolangfeed;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,10 +15,24 @@ public class FilterChooser extends AppCompatActivity{
     private TextView languageText, typeText, statusText;
     private Spinner languageSel, typeSel, statusSel;
 
+    //private final SharedPreferences sharedPref = getSharedPreferences(PRER_FILE_NAME, MODE_PRIVATE);
+    private SharedPreferences sharedPref;
+    private SharedPreferences.Editor editor;
+
+    private static final String LANG_SEL_POS = "lang_sel_pos";
+    private static final String TYPE_SEL_POS = "type_sel_pos";
+    private static final String STAT_SEL_POS = "stats_sel_pos";
+    //private static final String PRER_FILE_NAME = "spinner_options";
+
+    private static final int DEF_POS = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_chooser);
+
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = sharedPref.edit();
 
         languageText = (TextView) findViewById(R.id.language_select_text);
         typeText = (TextView) findViewById(R.id.user_type_select_text);
@@ -26,6 +42,14 @@ public class FilterChooser extends AppCompatActivity{
         typeSel = (Spinner) findViewById(R.id.user_type_select_spinner);
         statusSel = (Spinner) findViewById(R.id.status_select_spinner);
 
+        int langSelPos = sharedPref.getInt(LANG_SEL_POS, DEF_POS);
+        int typeSelPos = sharedPref.getInt(TYPE_SEL_POS, DEF_POS);
+        int statSelPos = sharedPref.getInt(STAT_SEL_POS, DEF_POS);
+
+        if (langSelPos != DEF_POS) languageSel.setSelection(langSelPos);
+        if (typeSelPos != DEF_POS) typeSel.setSelection(typeSelPos);
+        if (statSelPos != DEF_POS) statusSel.setSelection(statSelPos);
+
         languageText.setText(R.string.language_selector);
         typeText.setText(R.string.type_selector);
         statusText.setText(R.string.status_selector);
@@ -34,6 +58,8 @@ public class FilterChooser extends AppCompatActivity{
         typeSel.setAdapter(getAdapter(R.array.types));
         statusSel.setAdapter(getAdapter(R.array.statuses));
 
+
+
         languageSel.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -41,6 +67,8 @@ public class FilterChooser extends AppCompatActivity{
                 Feed.getUserFilter().setLanguage(Language.match(item));
 
                 ((Feed) Feed.getContext()).updateFilter();
+                editor.putInt(LANG_SEL_POS, languageSel.getSelectedItemPosition());
+                editor.apply();
             }
 
             @Override
@@ -55,6 +83,8 @@ public class FilterChooser extends AppCompatActivity{
                 Feed.getUserFilter().setType(UserType.match(item));
 
                 ((Feed) Feed.getContext()).updateFilter();
+                editor.putInt(TYPE_SEL_POS, typeSel.getSelectedItemPosition());
+                editor.apply();
             }
 
             @Override
@@ -69,6 +99,8 @@ public class FilterChooser extends AppCompatActivity{
                 Feed.getUserFilter().setStatus(Status.match(item));
 
                 ((Feed) Feed.getContext()).updateFilter();
+                editor.putInt(STAT_SEL_POS, statusSel.getSelectedItemPosition());
+                editor.apply();
             }
 
             @Override
--- app/src/main/java/jackah2/hellolangfeed/UserFilter.java
+++ app/src/main/java/jackah2/hellolangfeed/UserFilter.java
@@ -1,13 +1,17 @@
 package jackah2.hellolangfeed;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
  * Created by xmrpo on 10/20/2017.
  */
 
-public class UserFilter {
+public class UserFilter implements Parcelable{
 
     private Language language;
     private Status status;
@@ -54,34 +58,75 @@ public class UserFilter {
     }
 
     public List<User> filter(List<User> users){
-        List<User> filtered = new ArrayList<>();
+        List<User> filtered = new ArrayList<>(users);
 
         if(language == null && status == null && type == null){
-            filtered.addAll(users);
             return filtered;
         }
 
-        if(language != null){
-            for(User user : users){
-                if(user.getMainLanguage() == language)
-                    filtered.add(user);
-            }
-        }
+        Iterator<User> filterIt = filtered.iterator();
+        User current;
 
-        if(status != null){
-            for(User user : users){
-                if(user.getStatus() == status && !filtered.contains(user))
-                    filtered.add(user);
-            }
-        }
+        while (filterIt.hasNext()) {
 
-        if(type != null){
-            for(User user : users){
-                if(user.getType() == type && !filtered.contains(user))
-                    filtered.add(user);
+            current = filterIt.next();
+
+            if (language != null){
+                if (current.getMainLanguage() != language) {
+                    filterIt.remove();
+                    continue;
+                }
+            }
+
+            if (status != null){
+                if (current.getStatus() != status) {
+                    filterIt.remove();
+                    continue;
+                }
+            }
+
+            if (type != null) {
+                if (current.getType() != type) {
+                    filterIt.remove();
+                    continue;
+                }
             }
         }
 
         return filtered;
     }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.status == null ? -1 : this.status.ordinal());
+        dest.writeInt(this.language == null ? -1 : this.language.ordinal());
+        dest.writeInt(this.type == null ? -1 : this.type.ordinal());
+    }
+
+    protected UserFilter(Parcel in) {
+        int tmpStatus = in.readInt();
+        this.status = tmpStatus == -1 ? null : Status.values()[tmpStatus];
+        int tmpLanguage = in.readInt();
+        this.language = tmpLanguage == -1 ? null : Language.values()[tmpLanguage];
+        int tmpType = in.readInt();
+        this.type = tmpType == -1 ? null : UserType.values()[tmpType];
+    }
+
+    public static final Creator<UserFilter> CREATOR = new Creator<UserFilter>() {
+        @Override
+        public UserFilter createFromParcel(Parcel source) {
+            return new UserFilter(source);
+        }
+
+        @Override
+        public UserFilter[] newArray(int size) {
+            return new UserFilter[size];
+        }
+    };
 }
